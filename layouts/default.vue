bfcab9984ba7924957fc65ad53920049d49e681b
phte1100 <philip.telberg@gmail.com>
2025-05-19 08:53:58 +0200

    test

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
-  <!-- app.vue eller layout -->
-<NuxtHead>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
-</NuxtHead>
+  <NuxtHead>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
+  </NuxtHead>
 
-    <!-- Innehåll -->
+  <Navbar />
 
-      <Navbar />
-      <main>
-      <slot />
-    </main>
-    <Footer />
+  <!-- Gör main till min-h-screen, inte innehållet i sidorna -->
+  <main class="min-h-screen">
+    <slot />
+  </main>
+
+  <Footer />
 </template>
